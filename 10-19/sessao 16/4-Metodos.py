e4f32edbb19cf6ec62ffc60b8e0c99ae2c361ccd
MarcioVFM <marciov859@gmail.com>
2023-12-24 17:38:56 -0400

    att

--- 10-19/sessao 16/4-Metodos.py
+++ 10-19/sessao 16/4-Metodos.py
@@ -58,6 +58,11 @@ class Produto:
 
 
 class Usuario:
+    contador = 0
+
+    @classmethod
+    def conta_usuarios(cls):#cls e a classe toda, o Usuario
+        print(f'Temos {cls.contador} usuarios no sistema')
 
     def __init__(self, nome, sobrenome, email, senha):
         self.__nome = nome
@@ -102,6 +107,15 @@ else:
     print('acesso negado')
 
 print(f'Senha do usuario criptografada {user._Usuario__senha}')
+
+#Metodo de classe, o @classmethod
+#É usado quando voce quer criar uma funcao e nao que acessar nada da classe
+#tambem chamados de metodos estaticos
+user = Usuario('Marcio', 'Messias', 'sdaffasda2GMAIL.COM', '123')
+
+Usuario.conta_usuarios()
+user.conta_usuarios()
+print(Usuario.nome_completo(user))
 """
 from passlib.hash import pbkdf2_sha256 as cryp
 
@@ -141,50 +155,33 @@ class Produto:
 
 class Usuario:
 
+    contador = 0
+
+    @classmethod
+    def conta_usuarios(cls):#cls e a classe toda, o Usuario
+        print(f'Temos {cls.contador} usuarios no sistema')
+
     def __init__(self, nome, sobrenome, email, senha):
+        self.__id = Usuario.contador + 1
         self.__nome = nome
         self.__sobrenome = sobrenome
         self.__email = email
         self.__senha = cryp.hash(senha, rounds=20000, salt_size=16)
-    
+        Usuario.contador = self.__id
+        print(f'Usuario criado {self.gera_usuario()}')
     def nome_completo(self):
            #Retorna o nome completo do usuario
         return self.__nome + ' ' + self.__sobrenome
+    
     def checa_senha(self, senha):
         if cryp.verify(senha, self.__senha):
             return True
         return False
 
-p1 = Produto("PS5", "Console", 4000)
-print(p1.desconto(20))
+    def gera_usuario(self):
+        return self.__email.split('@')[0]
 
-u1 = Usuario('Marcio', 'Vinicius', 'marciov85@gmail.com', '12345')
-print(u1.nome_completo())
-
-nome = input('Informe o nome: ')
-sobrenome = input('informe o sobrenome:')
-email = input('informe o seu email: ')
-senha = input('informe sua senha: ')
-confirma_senha = input('confirme sua senha: ')
-
-if senha == confirma_senha:
-    user = Usuario(nome, sobrenome, email, senha)
-
-else:
-    print('A senha nao e a mesma')
-    exit(1)
-
-print('Usuario criado com sucesso')
-senha = input('Informe a sennha para acesso: ')
-
-if user.checa_senha(senha):
-    print('acesso permitido')
-
-else:
-    print('acesso negado')
-
-print(f'Senha do usuario criptografada {user._Usuario__senha}')
-
+user = Usuario('Marcio', 'Messias', 'sdaffasda@GMAIL.COM', '123')
 
 
 
